b5c3072016c034374c332569dd6524aeeacf53cf
Crowdbotics <team@crowdbotics.com>
2021-09-14 19:14:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mocktax_30544Identifier",
+  url: "https://mocktax_30544Identifier.botics.co",
+  api: "https://mocktax_30544Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
